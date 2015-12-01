df2d1902cd1939306bed0d7881ea81e833e83e3a
cymon343 <cymon343@hotmail.com>
2015-12-01 13:26:27 +0100

    Added DBCOnnectionFactory

---
+++ src/java/DB/DBConnectionFactory.java
@@ -0,0 +1,66 @@
+package DB;
+
+
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author SBL
+ */
+public class DBConnectionFactory 
+{
+    private String _user = System.getProperty("user.name");
+    private String _pw;
+
+    private static DBConnectionFactory _instance;
+    
+    private final String SERVER_NAME = "localhost";//"10.153.0.143"; //"localhost"; 
+    private final String DATABASE_INSTANCE = "SQLEXPRESS";
+    private final int PORTNO = 1433;//49197; // 58828; //1433;
+    private final String DATABASE_NAME = "dudeReg";
+    private final String USERNAME = "sa";
+    private final String PASSWORD = _pw;
+
+    private DBConnectionFactory() throws SQLServerException, SQLException 
+    {
+        if (!_user.equals("bruger"))
+        {_pw = "Brutalis";}
+        else
+        {_pw = "offlimit";}
+    }
+
+    /**
+     * Returns a new connection
+     *
+     * @return m_connection
+     */
+    public Connection getConnection() throws SQLException 
+    {
+        SQLServerDataSource ds = new SQLServerDataSource();
+        ds.setServerName(SERVER_NAME);
+        ds.setInstanceName(DATABASE_INSTANCE);
+        ds.setDatabaseName(DATABASE_NAME);
+        ds.setPortNumber(PORTNO);
+        ds.setUser(USERNAME);
+        ds.setPassword(PASSWORD);
+        
+        Connection c = ds.getConnection();
+        
+        if (!c.isClosed())   {System.out.println("Successfully connected to Database\n");}
+        
+        return c;
+    }
+
+    public static DBConnectionFactory getInstance() throws SQLException
+    {
+        if (_instance == null)
+        {
+            _instance = new DBConnectionFactory();
+        }
+        return _instance;
+    }
+    
+}
