d68c519385643cd0f62891ca04d160a6bae0f5db
cymon343 <cymon343@hotmail.com>
2015-12-03 10:11:09 +0100

    ServerFix
    Had glassfish issue

--- src/java/Services/AdminServices.java
+++ src/java/Services/AdminServices.java
@@ -5,14 +5,9 @@
  */
 package Services;
 
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.GET;
-import javax.ws.rs.Produces;
 
 /**
  * REST Web Service
