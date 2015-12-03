63b33391e40fb3c41bfd1e5e2db6d320c11f92a8
cymon343 <cymon343@hotmail.com>
2015-12-03 11:17:21 +0100

    fixed the consumes thingy

--- src/java/Services/UserServices.java
+++ src/java/Services/UserServices.java
@@ -5,16 +5,9 @@
  */
 package Services;
 
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.FormParam;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Produces;
 
 /**
  * REST Web Service
@@ -59,11 +52,10 @@ public class UserServices
     
     @Path("/registerparticipant")
     @POST
-    @Consumes("application/xlx")
     public void registerParticipant(@FormParam("fname") String fName, @FormParam("lname") String lName, 
                                     @FormParam("age") String age, @FormParam("email") String email)
     {
         //Testing connectivity
-        System.out.println(fName + "" + lName + " is " + age + " years old");
+        System.out.println(fName + " " + lName + " is " + age + " years old");
     }
 }
