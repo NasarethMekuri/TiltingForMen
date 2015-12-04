1552c36a08d204f11d992662677be4fc402fa9c5
cymon343 <cymon343@hotmail.com>
2015-12-04 13:35:53 +0100

    added failpage for registration

--- src/java/Services/UserServices.java
+++ src/java/Services/UserServices.java
@@ -1,8 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 package Services;
 
 import DB.DBHandler;
@@ -21,17 +21,17 @@ import javax.ws.rs.core.UriInfo;
 @Path("userservices")
 public class UserServices
 {
-
+    
     @Context
     private UriInfo context;
-
+    
     /**
      * Creates a new instance of UserServices
      */
     public UserServices()
     {
     }
-
+    
     /**
      * Retrieves representation of an instance of Services.UserServices
      * @return an instance of java.lang.String
@@ -43,7 +43,7 @@ public class UserServices
         //TODO return proper representation object
         throw new UnsupportedOperationException();
     }
-
+    
     /**
      * PUT method for updating or creating an instance of UserServices
      * @param content representation for the resource
@@ -56,37 +56,37 @@ public class UserServices
     
     @Path("/registerparticipant")
     @POST
-    public Response registerParticipant(@FormParam("fname") String fName, @FormParam("lname") String lName, 
-                                    @FormParam("age") String age, @FormParam("email") String email)
+    public Response registerParticipant(@FormParam("fname") String fName, @FormParam("lname") String lName,
+            @FormParam("age") String age, @FormParam("email") String email)
     {
-        //TODO: Do redirect or return succes/fail page here.
-
         //Testing connectivity
         //System.out.println(fName + " " + lName + " is " + age + " years old");
         
-        //Writing to the database.
-        if (!DBHandler.getInstance().hasParticipant(fName, lName, email)) 
+        boolean hasParticipant = DBHandler.getInstance().hasParticipant(fName, lName, email);
+        
+        String pageURL = "";
+        
+        if (!hasParticipant)
         {
+            //Writing to the database.
             DBHandler.getInstance().createParticipant(fName, lName, email, null, -1, Integer.parseInt(age), false, false);
+            pageURL = "http://127.0.0.1:8080/TiltingForMen/registration_success.html";
         }
         else
         {
-            //TODO: return registration_error page ().
+            pageURL = "http://127.0.0.1:8080/TiltingForMen/registration_fail.html";
         }
         
-        
-        URI confirmPage = null;
+        URI redirectPage = null;
         try
         {
-        confirmPage = new URI("http://127.0.0.1:8080/TiltingForMen/registration_success.html");
+            redirectPage = new URI(pageURL);
         }
         catch (URISyntaxException ex)
         {
-        System.out.println("Error:" + ex);
+            System.out.println("Error:" + ex);
         }
         
-        
-        return Response.temporaryRedirect(confirmPage).build();
-
+        return Response.temporaryRedirect(redirectPage).build();
     }
 }
