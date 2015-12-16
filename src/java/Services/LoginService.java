471c412084bd700bf72ed6f0ea025a2b534a1dd4
NasarethMekuri <tm_maniac@hotmail.com>
2015-12-16 12:33:30 +0100

    HAH! final refactoring!

--- src/java/Services/LoginService.java
+++ src/java/Services/LoginService.java
@@ -1,8 +1,3 @@
-/*
-* To change this license header, choose License Headers in Project Properties.
-* To change this template file, choose Tools | Templates
-* and open the template in the editor.
-*/
 package Services;
 
 import Persistence.DBHandler;
@@ -14,11 +9,6 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
-/**
- * REST Web Service
- *
- * @author bruger
- */
 @Path("loginservice")
 public class LoginService
 {
@@ -37,6 +27,7 @@ public class LoginService
     @POST
     public Response loginUser(@FormParam("username") String userName, @FormParam("pw") String pw)
     {
+        //TODO: Handle encoding of form param (ÆØÅ doesn't work)
         Pattern p = Pattern.compile("[a-zA-Z0-9]\\w{5,50}"); //Special characters are not allowed in password (including æøå ÆØÅ)
         Matcher compiledPassword = p.matcher(pw);
         String tryAgainURL = "http://127.0.0.1:8080/TiltingForMen/login_error.html";
